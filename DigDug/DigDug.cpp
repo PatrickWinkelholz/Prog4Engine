e569094c2d494dad458694515e52360323f28476
Patrick Winkelholz <patrick.winkelholz@student.howest.be>
2019-04-27 20:52:21 +0200

    split projects

---
+++ DigDug/DigDug.cpp
@@ -0,0 +1,19 @@
+#include "MiniginPCH.h"
+#include "Minigin.h"
+#include <chrono>
+#include <thread>
+#include "InputManager.h"
+#include "SceneManager.h"
+#include "Renderer.h"
+#include "ResourceManager.h"
+#include <SDL.h>
+#include "Texture.h"
+#include "GameObject.h"
+#include "Scene.h"
+#include "HudText.h"
+#include "FPSCounter.h"
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+}
